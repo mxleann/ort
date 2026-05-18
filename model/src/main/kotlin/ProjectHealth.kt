7ae3d6f41e5a034386eaf00c65ecd7e6824c354c
Leann Musterer <leann@Mac.fritz.box>
2026-05-18 19:33:28 +0200

    feature/#24_project_health_data_scheme

---
+++ model/src/main/kotlin/ProjectHealth.kt
@@ -0,0 +1,52 @@
+package org.ossreviewtoolkit.model
+
+enum class Criticality {
+    Low, Medium, High, Critical
+}
+
+
+data class ProjectHealth (
+        /**
+       * The name of project health metric.
+       */
+      val name: String? = null,
+
+
+      /**
+       * The metric score.
+       */
+    val score: Double? = null,
+
+    /**
+     * The metric crititcality.
+     */
+    val criticality: Criticality? = null,
+
+    /**
+     * The reason for the score.
+     */
+    val reason: String? = null,
+
+    /**
+     * Details about the findings.
+     */
+    val details: List<String>? = null,
+
+    /**
+     * Summary of documentation about the metric.
+     */
+    val documentation: String? = null,
+
+    /**
+     * Link to the documentation about the metric.
+     */
+    val documentationLink: String? = null,
+
+    /**
+     * Origin of evaluation.
+     */
+    val source: String
+){
+
+
+}
