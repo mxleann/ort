a234219cf134fd93d3ec947afe7ed4724313319f
Leann Musterer <leann@Mac.fritz.box>
2026-05-21 20:38:35 +0200

    feature/#24 add tests and fix some issues in Scorecard.kt

--- model/src/main/kotlin/ProjectHealth.kt
+++ model/src/main/kotlin/ProjectHealth.kt
@@ -11,10 +11,10 @@ data class ProjectHealth (
      */
     val name: String? = null,
 
-    /**
-     * The metric score.
-     */
-    val score: Double? = null,
+      /**
+       * The metric score.
+       */
+    val value: Double? = null,
 
     /**
      * The metric crititcality.
