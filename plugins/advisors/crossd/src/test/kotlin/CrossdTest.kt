021717a68719432e695c60b881c43a8edd778a4e
André Wille <115950185+andrewille@users.noreply.github.com>
2026-06-20 14:47:54 +0200

    chore: split tests into test/ and funTest/

--- plugins/advisors/crossd/src/test/kotlin/CrossdTest.kt
+++ plugins/advisors/crossd/src/test/kotlin/CrossdTest.kt
@@ -20,17 +20,9 @@
 package org.ossreviewtoolkit.plugins.advisors.crossd
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.collections.shouldBeEmpty
-import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 
 import org.ossreviewtoolkit.model.Criticality
-import org.ossreviewtoolkit.model.Identifier
-import org.ossreviewtoolkit.model.Package
-import org.ossreviewtoolkit.model.RemoteArtifact
-import org.ossreviewtoolkit.model.VcsInfo
-import org.ossreviewtoolkit.model.VcsType
 
 class CrossdTest : StringSpec({
     val testConfig = CrossdConfig(
@@ -40,106 +32,6 @@ class CrossdTest : StringSpec({
         thresholdCriticalityHigh = 30
     )
     val testThresholds = testConfig.getThresholds()
-    val crossd = Crossd(config = testConfig)
-
-    "retrievePackageFindings for a valid package should return findings" {
-        val pkg = Package(
-            id = Identifier("NPM::babel-code-frame:6.26.0"),
-            declaredLicenses = setOf("MIT"),
-            description = "Generate errors that contain a code frame that point to source locations.",
-            homepageUrl = "https://babeljs.io/",
-            binaryArtifact = RemoteArtifact.EMPTY,
-            sourceArtifact = RemoteArtifact.EMPTY,
-            vcs = VcsInfo(
-                type = VcsType.GIT,
-                url = "https://github.com/babel/babel/tree/master/packages/babel-code-frame",
-                revision = ""
-            ),
-            vcsProcessed = VcsInfo(
-                type = VcsType.GIT,
-                url = "https://github.com/babel/babel.git",
-                revision = "master"
-            )
-        )
-
-        val result = crossd.retrievePackageFindings(setOf(pkg))
-
-        result shouldNotBe null
-        result.keys.shouldNotBeEmpty()
-        result.values.first().advisor shouldBe crossd.details
-        result.values.first().projectHealth.shouldNotBeEmpty()
-    }
-
-    "retrievePackageFindings for an unknown package should return no findings but not crash" {
-        val pkg = Package(
-            id = Identifier("NPM::iojawdoiwajdiowajgdwaoijwadoiwadjwadowadji:1.33.7"),
-            declaredLicenses = setOf("MIT"),
-            description = "",
-            homepageUrl = "",
-            binaryArtifact = RemoteArtifact.EMPTY,
-            sourceArtifact = RemoteArtifact.EMPTY,
-            vcs = VcsInfo(
-                type = VcsType.GIT,
-                url = "https://github.com/iojawdoiwajdiowajgdwaoijwadoiwadjwadowadji/" +
-                    "test/tree/master/packages/whatever",
-                revision = ""
-            ),
-            vcsProcessed = VcsInfo(
-                type = VcsType.GIT,
-                url = "https://github.com/iojawdoiwajdiowajgdwaoijwadoiwadjwadowadji/test.git",
-                revision = "master"
-            )
-        )
-
-        val result = crossd.retrievePackageFindings(setOf(pkg))
-
-        result shouldNotBe null
-        result.keys shouldBe setOf(pkg)
-        result.values.first().advisor shouldBe crossd.details
-        result.values.first().projectHealth.shouldBeEmpty()
-    }
-
-    "retrievePackageFindings for a package with no vcs should return empty health data" {
-        val pkg = Package(
-            id = Identifier("Maven:org.apache.logging.log4j:log4j-api:2.14.1"),
-            declaredLicenses = emptySet(),
-            description = "",
-            homepageUrl = "",
-            binaryArtifact = RemoteArtifact.EMPTY,
-            sourceArtifact = RemoteArtifact.EMPTY,
-            vcs = VcsInfo.EMPTY,
-            vcsProcessed = VcsInfo.EMPTY
-        )
-
-        val result = crossd.retrievePackageFindings(setOf(pkg))
-
-        result.keys shouldBe setOf(pkg)
-        result.values.first().advisor shouldBe crossd.details
-        result.values.first().projectHealth.shouldBeEmpty()
-    }
-
-    "retrievePackageFindings for a malformed vcs url should return an issue" {
-        val pkg = Package(
-            id = Identifier("Maven:org.apache.logging.log4j:log4j-api:2.14.1"),
-            declaredLicenses = emptySet(),
-            description = "",
-            homepageUrl = "",
-            binaryArtifact = RemoteArtifact.EMPTY,
-            sourceArtifact = RemoteArtifact.EMPTY,
-            vcs = VcsInfo.EMPTY,
-            vcsProcessed = VcsInfo(
-                type = VcsType.GIT,
-                url = "https://invalid-url",
-                revision = "ddde192"
-            )
-        )
-
-        val result = crossd.retrievePackageFindings(setOf(pkg))
-
-        result.keys shouldBe setOf(pkg)
-        val issue = result.getValue(pkg).summary.issues.first()
-        issue.message shouldBe "The VCS URL 'https://invalid-url' could not be mapped to a repository."
-    }
 
     "higherIsBetterMetric should return the correct criticality" {
         val metric = CrossdMetric(
